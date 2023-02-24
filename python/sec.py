f163916fc6ca25311bc5995da863b96f2771a851
hunter morningstar <87934797+hunter666hz@users.noreply.github.com>
2023-02-24 21:18:38 +0100

    Add files via upload

---
+++ python/sec.py
@@ -0,0 +1,21 @@
+# #(w=overwrite? ,r=read,a=write in end )in sec p
+# name = input("enter your name ")
+# age = input("enter your age ")
+# work = input("enter your work ")
+
+
+
+
+# with open("start.txt","a") as e :
+# #readlines split(",")() of x.raed()
+# #
+#         e.write(name+" ")
+#         e.write(age+" ")
+#         e.write(work +" ")
+
+        
+
+    # e.write("test")
+
+max = [[12,"test","hello "],[5,"yoo","l"] ]
+print(max)
---
+++ python/start.py
@@ -0,0 +1,20 @@
+print("hello world")
+# name = input("give me your name ")
+# age= input("give me your name ")
+# def test(n , m):
+#     print("hello" , n , " did you knaw that your age is :" ,m ,"?")
+# test(name,age)
+
+#for i in range(start ,end ,step):
+#do this
+
+# for i in range(5):
+#     print(i)
+
+name = ["y","9","u can","put" ,"any","y",0,"want"]
+
+# k =input("add any value ")
+# name.append(k)
+# print(name)
+print(type(name[1]))
+print(int(name[1])+5)
